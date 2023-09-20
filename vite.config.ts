f6f46f0ac0608f337d592402dadb372c37e4720b
mortyhwk <jsphfrdg@gmail.com>
2023-09-20 12:47:06 +0200

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+    lib: {
+      entry: resolve(__dirname, 'src/index.tsx'),
+      name: 'progexp',
+      fileName: 'progexp',
+    },
+  },
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: "/src",
+    },
+  },
 })
